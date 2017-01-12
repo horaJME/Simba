fad467a1c98e4cb5995e41c92b4730030c14aeb8
Marko Horvat <marko.horvat5@fer.hr>
2017-01-12 21:45:15 +0100

    Final Project Commit
    Design finished, project backbone in place

--- Simba/AuthViewController.swift
+++ Simba/AuthViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
+var PIN2 = ""
+
 class AuthViewController: UIViewController {
 
     @IBOutlet weak var PINText: UITextField!
     
     @IBAction func SendPIN(_ sender: UIButton) {
-        
-        
+        PIN2 = PINText.text!
+        performSegue(withIdentifier: "SuccessSegue", sender: self)
     }
     
     override func viewDidLoad() {
