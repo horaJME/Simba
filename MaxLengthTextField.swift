ef2f94b4a9921f64450b97d61a93998196015771
Marko Horvat <marko.horvat5@fer.hr>
2017-01-16 22:30:15 +0100

    Add: New text restriction extension

---
+++ MaxLengthTextField.swift
@@ -0,0 +1,52 @@
+//
+//  MaxLengthTextField.swift
+//  Simba
+//
+//  Created by Infinum on 16/01/17.
+//  Copyright © 2017 Infinum. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+
+class MaxLengthTextField: UITextField, UITextFieldDelegate {
+    
+    private var characterLimit: Int?
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        delegate = self
+    }
+    
+    @IBInspectable var maxLength: Int {
+        get {
+            guard let length = characterLimit else {
+                return Int.max
+            }
+            return length
+        }
+        set {
+            characterLimit = newValue
+        }
+    }
+    
+    // 1
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // 2
+        guard string.characters.count > 0 else {
+            return true
+        }
+        
+        // 3
+        let currentText = textField.text ?? ""
+        // 4
+        let prospectiveText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        // 5
+        return prospectiveText.characters.count <= maxLength
+    }
+    
+}
